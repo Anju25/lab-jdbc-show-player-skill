da4540d563e59e0970d4be24cc83e757ddf7f0b3
Anju <anju02051998@gmail.com>
2020-10-09 15:39:52 +0530

    prograd

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,54 @@
+package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+//import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static List<Skill> listall=new ArrayList<Skill>();
+	public static void main(String args[]) throws NumberFormatException, IOException, ClassNotFoundException, SQLException
+	{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the total number of records");
+		int total=Integer.parseInt(br.readLine());
+		long id=0; String name=null;
+		SkillDAO skilldao=new SkillDAO();
+		for(int i=0;i<total;i++)
+		{
+			System.out.print("Enter the id of "+" "+i+"st:");
+			id=Long.parseLong(br.readLine());
+			System.out.print("Enter the name of "+" "+i+"st:");
+			name=br.readLine();
+			Skill s=new Skill(id,name);
+			skilldao.setdetails(s);
+		}
+		listall=skilldao.skills();
+		//Comparator<Skill> compareByName=(Skill o1,Skill o2)-> o1.getSkillname().compareTo(o2.getSkillname());
+	    //Collections.sort(listall,compareByName);
+		for(Skill s:listall)
+		{
+			System.out.println(s.getSkillid()+" "+s.getSkillname());
+		}
+		
+		/*for(int i=0;i<listall.size();i++)
+		{
+			 System.out.println(listall.get(i).getSkillid()+" "+listall.get(i).getSkillname());
+		}*/
+		
+		
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,49 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public static List<Skill> listall=new ArrayList<Skill>();
+	public  Skill setdetails(Skill s) throws ClassNotFoundException, SQLException
+	{
+		Connection con=ConnectionManager.getConnection();
+		if(con!=null)
+		System.out.println("established");
+		String sql="insert into player1 values(?,?)";
+		PreparedStatement st=con.prepareStatement(sql);
+		st.setLong(1,s.getSkillid());
+		st.setString(2, s.getSkillname());
+		st.executeUpdate();
+		st.close();
+		return s;
+		
+	}
+	public List<Skill> skills() throws ClassNotFoundException, SQLException
+	{
+		Connection con=ConnectionManager.getConnection();
+		if(con!=null)
+		System.out.println("established");
+		String sql="select * from player1 order by name asc";
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(sql);
+		while(rs.next())
+		{
+			Long id=rs.getLong(1);
+			String name=rs.getString(2);
+			Skill s=new Skill(id,name);
+			listall.add(s);
+		}
+		return listall;
+		
+	}
+}
